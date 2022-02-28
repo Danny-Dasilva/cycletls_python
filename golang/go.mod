eaaf7a5279433be31478f0c7dc71e1567437f381
Danny-Dasilva <yahchayildasilva@gmail.com>
2022-02-28 01:43:46 -0500

    WIP swap golang

---
+++ golang/go.mod
@@ -0,0 +1,13 @@
+module github.com/Danny-Dasilva/CycleTLS/cycletls
+
+go 1.14
+
+require (
+	github.com/Danny-Dasilva/fhttp v0.0.0-20211010093114-56fde831fe2f
+	github.com/andybalholm/brotli v1.0.3
+	github.com/gorilla/websocket v1.4.2
+	gitlab.com/yawning/utls.git v0.0.12-1
+	golang.org/x/crypto v0.0.0-20210921155107-089bfa567519 // indirect
+	golang.org/x/net v0.0.0-20220127200216-cd36cc0744dd
+	golang.org/x/text v0.3.7 // indirect
+)
---
+++ golang/index.go
@@ -0,0 +1,355 @@
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	http "github.com/Danny-Dasilva/fhttp"
+	"github.com/gorilla/websocket"
+	"io/ioutil"
+	"log"
+	"net/url"
+	"os"
+	"runtime"
+	"strings"
+	"time"
+)
+
+// Options sets CycleTLS client options
+type Options struct {
+	URL             string            `json:"url"`
+	Method          string            `json:"method"`
+	Headers         map[string]string `json:"headers"`
+	Body            string            `json:"body"`
+	Ja3             string            `json:"ja3"`
+	UserAgent       string            `json:"userAgent"`
+	Proxy           string            `json:"proxy"`
+	Cookies         []Cookie          `json:"cookies"`
+	Timeout         int               `json:"timeout"`
+	DisableRedirect bool              `json:"disableRedirect"`
+	HeaderOrder     []string          `json:"headerOrder"`
+	OrderAsProvided bool              `json:"orderAsProvided"` //TODO
+}
+
+type cycleTLSRequest struct {
+	RequestID string  `json:"requestId"`
+	Options   Options `json:"options"`
+}
+
+//rename to request+client+options
+type fullRequest struct {
+	req     *http.Request
+	client  http.Client
+	options cycleTLSRequest
+}
+
+//Response contains Cycletls response data
+type Response struct {
+	RequestID string
+	Status    int
+	Body      string
+	Headers   map[string]string
+}
+
+//JSONBody converts response body to json
+func (re Response) JSONBody() map[string]interface{} {
+	var data map[string]interface{}
+	err := json.Unmarshal([]byte(re.Body), &data)
+	if err != nil {
+		log.Print("Json Conversion failed " + err.Error() + re.Body)
+	}
+	return data
+}
+
+//CycleTLS creates full request and response
+type CycleTLS struct {
+	ReqChan  chan fullRequest
+	RespChan chan Response
+}
+
+func getWebsocketAddr() string {
+	port, exists := os.LookupEnv("WS_PORT")
+
+	var addr *string
+
+	if exists {
+		addr = flag.String("addr", "localhost:"+port, "http service address")
+	} else {
+		addr = flag.String("addr", "localhost:9112", "http service address")
+	}
+	u := url.URL{Scheme: "ws", Host: *addr, Path: "/"}
+
+	return u.String()
+}
+
+// ready Request
+func processRequest(request cycleTLSRequest) (result fullRequest) {
+
+	var browser = browser{
+		JA3:       request.Options.Ja3,
+		UserAgent: request.Options.UserAgent,
+		Cookies:   request.Options.Cookies,
+	}
+
+	client, err := newClient(
+		browser,
+		request.Options.Timeout,
+		request.Options.DisableRedirect,
+		request.Options.UserAgent,
+		request.Options.Proxy,
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	req, err := http.NewRequest(strings.ToUpper(request.Options.Method), request.Options.URL, strings.NewReader(request.Options.Body))
+	if err != nil {
+		log.Fatal(err)
+	}
+	headerorder := []string{}
+	//master header order, all your headers will be ordered based on this list and anything extra will be appended to the end
+	//if your site has any custom headers, see the header order chrome uses and then add those headers to this list
+	if len(request.Options.HeaderOrder) > 0 {
+		//lowercase headers
+		for _, v := range request.Options.HeaderOrder {
+			lowercasekey := strings.ToLower(v)
+			headerorder = append(headerorder, lowercasekey)
+		}
+	} else {
+		headerorder = append(headerorder,
+			"host",
+			"connection",
+			"cache-control",
+			"device-memory",
+			"viewport-width",
+			"rtt",
+			"downlink",
+			"ect",
+			"sec-ch-ua",
+			"sec-ch-ua-mobile",
+			"sec-ch-ua-full-version",
+			"sec-ch-ua-arch",
+			"sec-ch-ua-platform",
+			"sec-ch-ua-platform-version",
+			"sec-ch-ua-model",
+			"upgrade-insecure-requests",
+			"user-agent",
+			"accept",
+			"sec-fetch-site",
+			"sec-fetch-mode",
+			"sec-fetch-user",
+			"sec-fetch-dest",
+			"referer",
+			"accept-encoding",
+			"accept-language",
+			"cookie",
+		)
+	}
+
+	headermap := make(map[string]string)
+	//TODO: Shorten this
+	headerorderkey := []string{}
+	for _, key := range headerorder {
+		for k, v := range request.Options.Headers {
+			lowercasekey := strings.ToLower(k)
+			if key == lowercasekey {
+				headermap[k] = v
+				headerorderkey = append(headerorderkey, lowercasekey)
+			}
+		}
+
+	}
+
+	//ordering the pseudo headers and our normal headers
+	req.Header = http.Header{
+		http.HeaderOrderKey:  headerorderkey,
+		http.PHeaderOrderKey: {":method", ":authority", ":scheme", ":path"},
+	}
+	//set our Host header
+	u, err := url.Parse(request.Options.URL)
+	if err != nil {
+		panic(err)
+	}
+	req.Header.Set("Host", u.Host)
+
+	//append our normal headers
+	for k, v := range headermap {
+		req.Header.Set(k, v)
+	}
+
+	return fullRequest{req: req, client: client, options: request}
+
+}
+
+func dispatcher(res fullRequest) (response Response, err error) {
+	resp, err := res.client.Do(res.req)
+	if err != nil {
+
+		parsedError := parseError(err)
+
+		headers := make(map[string]string)
+		return Response{res.options.RequestID, parsedError.StatusCode, parsedError.ErrorMsg + "-> \n" + string(err.Error()), headers}, nil //normally return error here
+
+	}
+	defer resp.Body.Close()
+
+	encoding := resp.Header["Content-Encoding"]
+
+	bodyBytes, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Print("Parse Bytes" + err.Error())
+		return response, err
+	}
+	Body := DecompressBody(bodyBytes, encoding)
+	headers := make(map[string]string)
+
+	for name, values := range resp.Header {
+		if name == "Set-Cookie" {
+			headers[name] = strings.Join(values, "/,/")
+		} else {
+			for _, value := range values {
+				headers[name] = value
+			}
+		}
+	}
+	return Response{res.options.RequestID, resp.StatusCode, Body, headers}, nil
+
+}
+
+// Queue queues request in worker pool
+func (client CycleTLS) Queue(URL string, options Options, Method string) {
+
+	options.URL = URL
+	options.Method = Method
+	//TODO add timestamp to request
+	opt := cycleTLSRequest{"Queued Request", options}
+	response := processRequest(opt)
+	client.ReqChan <- response
+}
+
+// Do creates a single request
+func (client CycleTLS) Do(URL string, options Options, Method string) (response Response, err error) {
+
+	options.URL = URL
+	options.Method = Method
+	opt := cycleTLSRequest{"cycleTLSRequest", options}
+
+	res := processRequest(opt)
+	response, err = dispatcher(res)
+	if err != nil {
+		log.Print("Request Failed: " + err.Error())
+		return response, err
+	}
+
+	return response, nil
+}
+
+//TODO rename this
+
+// Init starts the worker pool or returns a empty cycletls struct
+func Init(workers ...bool) CycleTLS {
+	if len(workers) > 0 && workers[0] {
+		reqChan := make(chan fullRequest)
+		respChan := make(chan Response)
+		go workerPool(reqChan, respChan)
+		log.Println("Worker Pool Started")
+
+		return CycleTLS{ReqChan: reqChan, RespChan: respChan}
+	}
+	return CycleTLS{}
+
+}
+
+// Close closes channels
+func (client CycleTLS) Close() {
+	close(client.ReqChan)
+	close(client.RespChan)
+
+}
+
+// Worker Pool
+func workerPool(reqChan chan fullRequest, respChan chan Response) {
+	//MAX
+	for i := 0; i < 100; i++ {
+		go worker(reqChan, respChan)
+	}
+}
+
+// Worker
+func worker(reqChan chan fullRequest, respChan chan Response) {
+	for res := range reqChan {
+		response, err := dispatcher(res)
+		if err != nil {
+			log.Print("Request Failed: " + err.Error())
+		}
+		respChan <- response
+	}
+}
+
+func readSocket(reqChan chan fullRequest, c *websocket.Conn) {
+	for {
+		_, message, err := c.ReadMessage()
+		if err != nil {
+			log.Print("Socket Error", err)
+			return
+		}
+		request := new(cycleTLSRequest)
+
+		err = json.Unmarshal(message, &request)
+		if err != nil {
+			log.Print("Unmarshal Error", err)
+			return
+		}
+		log.Println(request)
+
+
+		reply := processRequest(*request)
+
+		reqChan <- reply
+	}
+}
+
+func writeSocket(respChan chan Response, c *websocket.Conn) {
+	for {
+		select {
+		case r := <-respChan:
+			message, err := json.Marshal(r)
+			if err != nil {
+				log.Print("Marshal Json Failed" + err.Error())
+				continue
+			}
+			err = c.WriteMessage(websocket.TextMessage, message)
+			if err != nil {
+				log.Print("Socket WriteMessage Failed" + err.Error())
+				continue
+			}
+
+		}
+
+	}
+}
+
+func main() {
+
+	runtime.GOMAXPROCS(runtime.NumCPU())
+
+	start := time.Now()
+	defer func() {
+		log.Println("Execution Time: ", time.Since(start))
+	}()
+
+	websocketAddress := getWebsocketAddr()
+	c, _, err := websocket.DefaultDialer.Dial(websocketAddress, nil)
+	if err != nil {
+		log.Print(err)
+		return
+	}
+
+	reqChan := make(chan fullRequest)
+	respChan := make(chan Response)
+	go workerPool(reqChan, respChan)
+
+	go readSocket(reqChan, c)
+	//run as main thread
+	writeSocket(respChan, c)
+
+}
