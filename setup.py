28a800ca6ecf28d79c8b483a50162d36d75ee526
Danny-Dasilva <yahchayildasilva@gmail.com>
2022-03-05 20:12:03 -0500

    other request types

--- setup.py
+++ setup.py
@@ -5,10 +5,10 @@ setup(
     version='0.0.1',
     packages=find_packages(exclude=['tests*']),
     license='none',
-    description='An example python package with functions simulating depositing and withdrawing cash from a bank account',
+    description='A python package for spoofing TLS',
     long_description=open('README.md').read(),
     install_requires=[],
-    url='REPOSITORY_URL',
-    author='AUTHOR_NAME',
-    author_email='AUTHOR_EMAIL'
+    url='github.com/Danny-Dasilva/cycletls_python',
+    author='Danny-Dasilva',
+    author_email='dannydasilva.solutions@gmail.com'
 )
